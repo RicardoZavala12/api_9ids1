457994e6116e1a6db2731a65b0704b29da6f4ca4
RicardoZavala <rickyesc1202011@gmail.com>
2024-02-13 00:35:14 -0600

    Correccion y prueba de API's

--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,14 @@ Route::post('login',[LoginController::class,'login']);
 Route::post('permisos/guardar',[PermisosController::class, 'store']);
 Route::post('permisos/eliminar',[PermisosController::class, 'delete']);
 
-Route::post('divisiones/guardar',[DivisionController::class, 'store']);
-Route::post('divisiones/eliminar',[DivisionController::class, 'delete']);
+Route::post('divisiones/guardar',[DivisionController::class, 'storeAPI']);
+Route::post('divisiones/eliminar',[DivisionController::class, 'deleteAPI']);
 
-Route::post('profesores/guardar',[ProfesorController::class, 'store']);
-Route::post('profesores/eliminar',[ProfesorController::class, 'delete']);
+Route::post('profesores/guardar',[ProfesorController::class, 'storeAPI']);
+Route::post('profesores/eliminar',[ProfesorController::class, 'deleteAPI']);
 
-Route::post('puestos/guardar',[PuestoController::class, 'store']);
-Route::post('puestos/eliminar',[PuestoController::class, 'delete']);
+Route::post('puestos/guardar',[PuestoController::class, 'storeAPI']);
+Route::post('puestos/eliminar',[PuestoController::class, 'deleteAPI']);
 
 
 
